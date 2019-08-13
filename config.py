a39c82947e45863179582f57a156f8f3358a229b
sylviah <sylviah.rutto@gmail.com>
2019-08-13 11:13:30 +0300

    deployment

--- config.py
+++ config.py
@@ -3,19 +3,19 @@ import os
 class Config:
 
    
-    SECRET_KEY = ('cossie')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     SQLALCHEMY_TRACK_MODIFICATIONS=True
-    MAIL_USERNAME = 'sylviah.rutto@gmail.com'
-    MAIL_PASSWORD = 'mulu@gift'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
 class ProdConfig(Config):
-     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://sylviah:sly@localhost/insight'
+     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://sylviah:sly@localhost/insight_test'
     QUOTES_API='http://quotes.stormconsultancy.co.uk/random.json'
